0e8facf5fde55d09e3b6e03a97a73728e582de49
Aarni <aarni.miikkulainen@helsinki.fi>
2023-06-30 16:20:41 +0300

    tuotantoversio

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 morgan.token('body', (req) => { return JSON.stringify(req.body)})
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
@@ -83,7 +86,7 @@ app.get('/info', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
