9a4c7277a92a345f7b5209600ae8998a08e3900f
Aarni <aarni.miikkulainen@helsinki.fi>
2023-09-23 17:02:14 +0300

    mongo connection fix

--- mongo.js
+++ mongo.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
-const password = process.argv[2]
-const url = `mongodb+srv://fullstack:${password}@full-stack.vvx6qip.mongodb.net/Phonebook?retryWrites=true&w=majority`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
